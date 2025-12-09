eed2cf4c623de5695804f40381096dffc876a0d7
shihabksaleem <shihab.ks56@gmail.com>
2025-12-09 22:14:00 +0400

    build release 9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2236258699"
+    serviceWorkerVersion: "2419710572"
   }
 });
